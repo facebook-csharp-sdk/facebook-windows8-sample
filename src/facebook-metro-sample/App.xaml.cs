fc15188fd6d0d5ee787f5b3491891bdf39bbd0d7
prabirshrestha <mail@prabir.me>
2012-04-27 22:22:25 -0400

    renamed to MainPage.xaml.cs

--- src/facebook-metro-sample/App.xaml.cs
+++ src/facebook-metro-sample/App.xaml.cs
@@ -48,7 +48,7 @@ namespace facebook_metro_sample
 
             // Create a Frame to act navigation context and navigate to the first page
             var rootFrame = new Frame();
-            rootFrame.Navigate(typeof(BlankPage));
+            rootFrame.Navigate(typeof(MainPage));
 
             // Place the frame in the current Window and ensure that it is active
             Window.Current.Content = rootFrame;
--- src/facebook-metro-sample/MainPage.xaml.cs
+++ src/facebook-metro-sample/MainPage.xaml.cs
@@ -19,9 +19,9 @@ namespace facebook_metro_sample
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class BlankPage : Page
+    public sealed partial class MainPage : Page
     {
-        public BlankPage()
+        public MainPage()
         {
             this.InitializeComponent();
         }
