e8d222c59c2aabb22b4a7324b68b99dfc4f99fe2
Nathan Totten <ntotten@microsoft.com>
2012-11-15 13:12:15 -0800

    Updated sample to use WebAuthenticationBroker

--- src/Facebook.Samples.Windows8/Views/HomePage.xaml.cs
+++ src/Facebook.Samples.Windows8/Views/HomePage.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Security.Authentication.Web;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,6 +23,12 @@ namespace Facebook.Samples.Views
     /// </summary>
     public sealed partial class HomePage : Page
     {
+
+        string _facebookAppId = "" // You must set your own AppId here
+        string _permissions = "user_about_me,read_stream,publish_stream"; // Set your permissions here
+
+        FacebookClient _fb = new FacebookClient();
+
         public HomePage()
         {
             this.InitializeComponent();
@@ -32,12 +40,68 @@ namespace Facebook.Samples.Views
         /// <param name="e">Event data that describes how this page was reached.  The Parameter
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
+        {  
         }
 
-        private void btnFacebookLogin_Click(object sender, RoutedEventArgs e)
+        private async void btnFacebookLogin_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(FacebookLoginPage));
+            var redirectUrl = "https://www.facebook.com/connect/login_success.html";
+            try
+            {
+                //fb.AppId = facebookAppId;
+                var loginUrl = _fb.GetLoginUrl(new
+                {
+                    client_id = _facebookAppId,
+                    redirect_uri = redirectUrl,
+                    scope = _permissions,
+                    display = "popup",
+                    response_type = "token"
+                });
+
+                var endUri = new Uri(redirectUrl);
+
+                WebAuthenticationResult WebAuthenticationResult = await WebAuthenticationBroker.AuthenticateAsync(
+                                                        WebAuthenticationOptions.None,
+                                                        loginUrl,
+                                                        endUri);
+                if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
+                {
+                    var callbackUri = new Uri(WebAuthenticationResult.ResponseData.ToString());
+                    var facebookOAuthResult = _fb.ParseOAuthCallbackUrl(callbackUri);
+                    var accessToken = facebookOAuthResult.AccessToken;
+                    LoginSucceded(accessToken);
+
+                }
+                else if (WebAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+                {
+                    throw new InvalidOperationException("HTTP Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseErrorDetail.ToString());
+                }
+                else
+                {
+                    throw new InvalidOperationException("Error returned by AuthenticateAsync() : " + WebAuthenticationResult.ResponseStatus.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                //
+                // Bad Parameter, SSL/TLS Errors and Network Unavailable errors are to be handled here.
+                //
+                throw ex;
+            }
+        }
+
+        private async void LoginSucceded(string accessToken)
+        {
+            dynamic parameters = new ExpandoObject();
+            parameters.access_token = accessToken;
+            parameters.fields = "id";
+
+            dynamic result = await _fb.GetTaskAsync("me", parameters);
+            parameters = new ExpandoObject();
+            parameters.id = result.id;
+            parameters.access_token = accessToken;
+
+            Frame.Navigate(typeof(FacebookInfoPage), (object)parameters);
         }
     }
 }
